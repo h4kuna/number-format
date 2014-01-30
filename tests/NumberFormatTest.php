a3f8a0903d6d4d824aa83dfa255a302b548136c5
Milan Matejcek <milan.matejcek@gmail.com>
2014-01-30 10:35:39 +0100

    Test NumberFormat

---
+++ tests/NumberFormatTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests;
+
+use h4kuna\INumberFormat;
+use h4kuna\NumberFormat;
+use PHPUnit_Framework_TestCase;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * @author Milan Matějček
+ */
+class NumberFormatTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     *
+     * @return NumberFormat
+     */
+    private function createNumberFormat() {
+        $number = new NumberFormat('CZK');
+        $number->setNumber(1234.567899);
+        $number->setNbsp(FALSE);
+        return $number;
+    }
+
+    public function testDecimal() {
+        $number = $this->createNumberFormat();
+        $number->setDecimal(1);
+        $this->assertEquals('1 234,6 CZK', (string) $number);
+
+        $number->setDecimal(-2);
+        $this->assertEquals('1 200 CZK', (string) $number);
+    }
+
+    public function testThousand() {
+        $number = $this->createNumberFormat();
+        $number->setThousand('-');
+        $this->assertEquals('1-234,57 CZK', (string) $number);
+    }
+
+    public function testMask() {
+        $number = $this->createNumberFormat();
+        $number->setMask('S 1');
+        $this->assertEquals('CZK 1 234,57', (string) $number);
+    }
+
+    public function testPoint() {
+        $number = $this->createNumberFormat();
+        $number->setPoint(';');
+        $this->assertEquals('1 234;57 CZK', (string) $number);
+    }
+
+    public function testSymbol() {
+        $number = $this->createNumberFormat();
+        $number->setSymbol('Kč');
+        $this->assertEquals('1 234,57 Kč', (string) $number);
+    }
+
+    public function testRender() {
+        $number = $this->createNumberFormat();
+        $this->assertEquals(NULL, $number->render());
+    }
+
+    public function testNbsp() {
+        $number = $this->createNumberFormat();
+        $number->setNbsp(TRUE);
+        $this->assertEquals('1' . INumberFormat::NBSP . '234,57' . INumberFormat::NBSP . 'CZK', (string) $number);
+    }
+
+    public function testZeroClear() {
+        $number = $this->createNumberFormat();
+        $number->setZeroClear(TRUE);
+        $number->setDecimal(5);
+        $this->assertEquals('1 234,5679 CZK', (string) $number);
+    }
+
+}
