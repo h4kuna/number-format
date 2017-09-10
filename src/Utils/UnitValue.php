64b8f61258bfa1fae5d1d6a73222e654d99af464
Milan Matějček <milan.matejcek@gmail.com>
2017-09-10 21:15:59 +0200

    Add dependency on Immutable\Messenger

--- src/Utils/UnitValue.php
+++ src/Utils/UnitValue.php
@@ -6,7 +6,7 @@ namespace h4kuna\Number\Utils;
  * @property-read float $value
  * @property-read string $unit
  */
-final class UnitValue extends Crate
+final class UnitValue extends \h4kuna\DataType\Immutable\Messenger
 {
 	public function __toString()
 	{
