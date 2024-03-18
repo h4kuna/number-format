101d854424e2ae71d4dab80f4dd54e97dbad15fb
Milan Matějček <milan.matejcek@gmail.com>
2024-03-18 08:22:18 +0100

    feat(Formatter): add __invoke method

--- src/Date/Formatter.php
+++ src/Date/Formatter.php
@@ -4,7 +4,13 @@ namespace h4kuna\Format\Date;
 
 use DateTimeInterface;
 
+/**
+ * methods format and __invoke keep same parameters and return type
+ */
 interface Formatter
 {
 	function format(?DateTimeInterface $dateTime): string;
+
+
+	function __invoke(?DateTimeInterface $dateTime): string;
 }
--- src/Number/Formatter.php
+++ src/Number/Formatter.php
@@ -2,7 +2,13 @@
 
 namespace h4kuna\Format\Number;
 
+/**
+ * methods format and __invoke keep same parameters and return type
+ */
 interface Formatter
 {
 	function format(string|int|float|null $number): string;
+
+
+	function __invoke(string|int|float|null $number): string;
 }
